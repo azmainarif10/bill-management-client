5b65332525a42d4c7dfec4c7bcc5c18598425ded
azmainarif10 <azmainarif10@gmail.com>
2025-11-13 22:50:31 +0600

    final change

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -28,20 +28,22 @@ if(loading){
                   <img src="/Payment.png" alt="" className="h-10 w-20 "/>
       <p className=" font-medium text-lg  text-indigo-400">BILL MANAGEMENT</p>
               </div>
+              
             <div className="navbar bg-base-100 shadow-sm">
   <div className="navbar-start">
-    <div className=" hidden md:flex items-center  gap-2 sm:gap-3">
+          <div className=" hidden md:flex items-center  gap-2 sm:gap-3">
       <img src="/Payment.png" alt="" className="h-20 w-40  "/>
       <p className=" font-bold text-xl  text-indigo-400">BILL MANAGEMENT</p>
     </div>
+
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
       </div>
       <ul
         tabIndex="-1"
-        className="menu menu-sm dropdown-content text-xl bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-          <li className="text-xl text-violet-400 font-medium"><Link to={'/'}>Home</Link></li> 
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li className="text-xl text-violet-400 font-medium"><Link to={'/'}>Home</Link></li> 
           <li className="text-xl text-violet-400 font-medium"><Link to={'/bills'}>Bills</Link></li> 
           <li className="text-xl text-violet-400 font-medium"><Link to={'/add-bills'}>Add Bills</Link></li> 
         
@@ -55,18 +57,20 @@ if(loading){
         user &&  <li className="text-xl text-violet-400 font-medium"><Link to={'/my-bills'}>My Pay Bill </Link></li> 
 
        }
+       
 
-      <div className="py-3">
+           <div className="py-3">
  {
   user && user.photoURL ?( <img  className='w-10 h-10 rounded-full' src={user.photoURL}></img>  ) : 
   ( <img  className='w-10 h-full rounded-full' src='https://i.ibb.co.com/gL19M6xH/icon-7797704-640.png'></img>  )
  }
    </div>
-       
-   
-        
       </ul>
     </div>
+    
+   
+       
+   
   
   </div>
   <div className="navbar-center hidden lg:flex">
